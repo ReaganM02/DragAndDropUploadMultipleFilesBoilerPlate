5734eb32ab4938fd2da42ec30fc289ee0a1fe8ec
Reagan Mahinay <reaganmahinay.web@gmail.com>
2024-02-16 08:01:45 +0800

    first commit

---
+++ components/FileDialog.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+</script>
+<template>
+  <div class="pointer-events-none grid place-items-center">
+    <div class="font-semibold text-cyan-500">
+      Drag & Drop Files Here
+    </div>
+    <div class="font-semibold text-cyan-400 my-2">
+      -OR-
+    </div>
+    <div class="font-semibold text-cyan-500">
+      Browse Files
+    </div>
+  </div>
+</template>
+<style scoped lang="css"></style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+useHead({
+  bodyAttrs: {
+    class: 'bg-cyan-300'
+  }
+})
+</script>
+<template>
+  <div class="p-6">
+    <div
+      class="max-w-[900px] w-full border-dashed border-2 border-cyan-400 rounded-md p-4 text-lg font-medium text-center transition bg-cyan-200 cursor-pointer"
+    >
+      <FileDialog />
+    </div>
+    <div class="mt-4">
+      <div class="text-cyan-700 text-lg">
+        Preview
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped lang="css"></style>
